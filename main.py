c821da383c910b97f19b346a6684c334a16f3eec
heechang.kwon <facee22av@naver.com>
2021-07-12 23:18:02 +0900

    [modify] convert list to DataFrame

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import re
 import pandas as pd
 from bs4 import BeautifulSoup
 
+pd.options.display.float_format = '{:.2f}'.format #소수 둘째까지 
 
-code = "035720"
+code = "005930"
 
 #요청할때마다 encparam, id가 필요하기 때문에 추출
 re_enc = re.compile("encparam: '(.*)'", re.IGNORECASE) 
@@ -25,16 +26,15 @@ html = pd.read_html(html)
 data_table = html[1]
 
 
+#컬럼명 년도만 추출
 df_columns = list(map(lambda x: ' '.join(x[1:]),data_table.columns))
+df_columns = [column[0:4] for column in df_columns]
 data_table.columns = df_columns
 
+#매출액, 영업이익, 당기순이익, PER, PBR, 현금배당수익률
+data_table = data_table.loc[[0,1,4,26,28,30],]
 
-매출액 = data_table.loc[0,:]
-영업이익 = data_table.loc[1,:]
-당기순이익 = data_table.loc[4,:]
-PER = data_table.loc[26,:]
-PBR = data_table.loc[28,:]
-현금배당수익율 = data_table.loc[30,:]
+print(data_table)
 
 
 #부채비율, 당좌비율, 자본유보율
@@ -47,14 +47,15 @@ json = requests.get(url, headers=headers).json()
 for dic in json['DATA']:
     if dic['ACC_NM'] == '부채비율':
         부채비율 = []
+        부채비율.append('부채비율')
         부채비율.append(dic['DATA1'])
         부채비율.append(dic['DATA2'])
         부채비율.append(dic['DATA3'])
         부채비율.append(dic['DATA4'])
         부채비율.append(dic['DATA5'])
-        부채비율.append(dic['DATA6'])
     if dic['ACC_NM'] == '당좌비율':
         당좌비율 = []
+        당좌비율.append('당좌비율')
         당좌비율.append(dic['DATA1'])
         당좌비율.append(dic['DATA2'])
         당좌비율.append(dic['DATA3'])
@@ -62,16 +63,13 @@ for dic in json['DATA']:
         당좌비율.append(dic['DATA5'])     
     if dic['ACC_NM'] == '자본유보율':
         자본유보율 = []
+        자본유보율.append('자본유보율')
         자본유보율.append(dic['DATA1'])
         자본유보율.append(dic['DATA2'])
         자본유보율.append(dic['DATA3'])
         자본유보율.append(dic['DATA4'])
         자본유보율.append(dic['DATA5'])
-
-print(부채비율)
-print(당좌비율)
-print(자본유보율)
-
+        
 #매출채권회전율, 재고자산회전율
 url = "http://companyinfo.stock.naver.com/company/cF4002.aspx?cmp_cd={}&frq=0&rpt=4&finGubun=MAIN&frqTyp=0&cn=&encparam={}".format(code, encparam)
 headers = {"Referer": "HACK"}
@@ -80,6 +78,7 @@ json = requests.get(url, headers=headers).json()
 for dic in json['DATA']:
     if dic['ACC_NM'] == '매출채권회전율':
         매출채권회전율 = []
+        매출채권회전율.append('매출채권회전율')
         매출채권회전율.append(dic['DATA1'])
         매출채권회전율.append(dic['DATA2'])
         매출채권회전율.append(dic['DATA3'])
@@ -87,29 +86,13 @@ for dic in json['DATA']:
         매출채권회전율.append(dic['DATA5'])
     if dic['ACC_NM'] == '재고자산회전율':
         재고자산회전율 = []
+        재고자산회전율.append('재고자산회전율')
         재고자산회전율.append(dic['DATA1'])
         재고자산회전율.append(dic['DATA2'])
         재고자산회전율.append(dic['DATA3'])
         재고자산회전율.append(dic['DATA4'])
         재고자산회전율.append(dic['DATA5'])
-        
-        
-print(매출채권회전율)
-print(재고자산회전율)
 
+df = pd.DataFrame([부채비율, 당좌비율, 자본유보율, 매출채권회전율, 재고자산회전율], columns = df_columns[0:6])
 
-import pymysql
-
-conn = None
-cur = None
-
-sql="desc BASIC_STACK_INFO"
-
-conn = pymysql.connect(host="springboot2-webservice.ctsnx9zgivip.ap-northeast-2.rds.amazonaws.com",
-                      user="admin",
-                      password="",
-                      db='stackinfo',
-                      charset='utf8')
-cur = conn.cursor()
-print(cur.execute(sql))
-
+data_table.append(df, sort=False)
